238d7a147a057470841b5b60d1baa2841124ce27
Joshua James <burke.joshua.james@gmail.com>
2017-09-14 03:49:21 +0000

    automatically backed up by learn

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -1,9 +1,6 @@
 def position_taken?(board, index)
   # board[index].nil? || board[index] == ' '
   move = board[index]
-  if move == ' ' || move == '' || move.nil?
-    false
-  else
-    true
-  end
+  return false if move == ' ' || move == '' || move.nil?
+  true
 end
